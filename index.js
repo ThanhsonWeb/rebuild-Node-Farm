2c3b09c1bd63d79fb1d7bfa5cf385f879c70daf3
ThanhsonWeb <sondeptroi@gmail.com>
2026-06-26 08:04:45 +0700

    filling teamplates

--- index.js
+++ index.js
@@ -2,17 +2,42 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-// create web server using Nodejs
 
+const replaceTemplate = (temp, el) => {
+	let output = temp.replace(/{%PRODUCTNAME%}/g, el.productName);
+	output = output.replace(/{%IMAGE%}/g, el.image);
+	output = output.replace(/{%PRICE%}/g, el.price);
+	output = output.replace(/{%FROM%}/g, el.from);
+	output = output.replace(/{%NUTRIENTS%}/g, el.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, el.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, el.description);
+	output = output.replace(/{%ID%}/g, el.id);
+	return output;
+};
+// read file synchronous
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	"utf-8",
+);
+const tempCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	"utf-8",
+);
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	"utf-8",
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
-console.log(dataObject);
 
+// ------------------ server------------------
 const server = http.createServer((req, res) => {
 	const pathname = req.url;
 
 	if (pathname === "/" || pathname === "/overview") {
-		res.end("Overview Page");
+		const cardsContent = dataObject.map((el) => replaceTemplate(tempCard, el));
+		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsContent);
+		res.end(output);
 	} else if (pathname === "/product") {
 		res.end("Product Page");
 	} else if (pathname === "/api") {
